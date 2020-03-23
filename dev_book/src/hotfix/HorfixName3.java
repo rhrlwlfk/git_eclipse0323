9b3aff91f66f66ef053abb435f30c88f3f0ae27c
rhrlwlfk <tkdguszz123@naver.com>
2020-03-23 16:43:12 +0900

    8/59

---
+++ dev_book/src/hotfix/HorfixName3.java
@@ -0,0 +1,9 @@
+package hotfix;
+
+public class HorfixName3 {
+
+	public static void main(String[] args) {
+		//프로젝트명 -> 오른쪽 - >Remote->switch- > new Branch - >hotfix
+		System.out.println("hotfix 브랜치에서 추가한 소스임");
+	}
+}
